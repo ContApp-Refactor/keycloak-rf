42005592f4e40b1f300dc54869f26a2370e1d65e
JulianRuano <julianrmajin@gmail.com>
2024-06-13 19:30:56 -0500

    feat: Agregar documentación y configuración de Keycloak en Docker Compose

---
+++ DOCUMENTATION.md
@@ -0,0 +1,6 @@
+## Keycloak
+
+
+
+![Escenario](https://res.cloudinary.com/dilrruxyx/image/upload/v1718310860/keycloak_pdryin.jpg)
+
---
+++ src/main/java/com/security/keycloak/infraestructure/security/IJwtUtils.java
@@ -0,0 +1,14 @@
+package com.security.keycloak.infraestructure.security;
+
+/**
+ * Utilidades para JWT
+*/
+public interface IJwtUtils {
+    
+    /**
+     * Obtiene el id del usuario
+     * @return id del usuario
+     */
+    String getId();
+    
+} 
