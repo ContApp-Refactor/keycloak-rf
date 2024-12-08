a513df52d5e7f040aca4fb62df4ae863d4e174dd
jhonmaroCIpia <jhonmaro@unicauca.edu.co>
2024-12-08 13:24:17 -0500

    Spring 3: Documentación de micro-servicio keycloak

--- src/main/java/com/security/keycloak/infraestructure/input/rest/data/response/UserResponse.java
+++ src/main/java/com/security/keycloak/infraestructure/input/rest/data/response/UserResponse.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import io.swagger.v3.oas.annotations.media.Schema; 
 
 @AllArgsConstructor
 @Getter
@@ -15,13 +15,17 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 public class UserResponse{
-
+    @Schema(description = "Identificador", example = "1")
     String id;
+    @Schema(description = "Nombre de usuario", example = "admin")
     String username;
+    @Schema(description = "Correo electrónico", example = "tuemail@example.com")
     String email;
+    @Schema(description = "Nombre", example = "Admin")
     String firstName;
+    @Schema(description = "Apellido", example = "Admin")
     String lastName;
-
+    @Schema(description = "Roles", example = "[\"admin\"]")
     List<String> roles;
 
 }
